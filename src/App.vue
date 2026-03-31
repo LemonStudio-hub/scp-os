c8b1553b7e31130ef903907528e43ba3f3c07533
SCP Foundation <scpos@foundation.scp>
2026-03-31 19:43:57 +0000

    Initial commit: SCP Foundation terminal system with xterm.js and Hammer.js

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import SCPTerminal from './components/SCPTerminal.vue'
+</script>
+
+<template>
+  <SCPTerminal />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: 'Courier New', Consolas, monospace;
+  background: #0a0a0a;
+  color: #ffffff;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
