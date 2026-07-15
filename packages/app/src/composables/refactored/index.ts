aec278ab22aea6315fa9cbfbe71a516dd8c6f970
daum110715 <daum110715@gmail.com>
2026-07-15 12:58:29 +0800

    chore(app): remove unused refactored composable stubs
    Delete dead use*Refactored composables that are no longer imported.

--- packages/app/src/composables/refactored/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Refactored Composables
- * Refactored composables using dependency injection and event-driven architecture
- */
-
-export * from '../useCommandHistoryRefactored'
-export * from '../useTerminalRefactored'
-export * from '../useTabsRefactored'
